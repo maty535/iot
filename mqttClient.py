7018bdb7fedc3dc37cc8854ecaa8fe009a03ae93
Matus Ivanecky <matus.ivanecky@4q.sk>
2017-01-06 15:58:53 +0100

    Added local DB mongo support

--- mqttClient.py
+++ mqttClient.py
@@ -1,17 +1,44 @@
-import datetime
+#!/usr/bin/env python3
 import paho.mqtt.client as mqtt
+import datetime
+from pymongo import MongoClient
+
 
 def on_connect(client, userdata, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("home/#")
 
 def on_message(client, userdata, msg):
-    print(str(datetime.datetime.now()) + ": " + msg.topic + " " + str(msg.payload))
+    receiveTime=datetime.datetime.now()
+    message=msg.payload.decode("utf-8")
+    isfloatValue=False
+    try:
+        # Convert the string to a float so that it is stored as a number and not a string in the database
+        val = float(message)
+        isfloatValue=True
+    except:
+        isfloatValue=False
 
+    if isfloatValue:
+        print(str(receiveTime) + ": " + msg.topic + " " + str(val))
+        post={"time":receiveTime,"topic":msg.topic,"value":val}
+    else:
+        print(str(receiveTime) + ": " + msg.topic + " " + message)
+        post={"time":receiveTime,"topic":msg.topic,"value":message}
+
+    collection.insert_one(post)
+
+
+# Set up client for MongoDB
+mongoClient=MongoClient()
+db=mongoClient.SensorData
+collection=db.home_data
+
+# Initialize the client that should connect to the Mosquitto broker
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-
 client.connect("192.168.3.4", 1883, 60)
 
+# Blocking loop to the Mosquitto broker
 client.loop_forever()
