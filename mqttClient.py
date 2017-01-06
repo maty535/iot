4bb1b6565428082b6a3ddcec888d2df7cec9bf53
Matus Ivanecky <maty535@users.noreply.github.com>
2017-01-06 15:26:03 +0100

    Create mqttClient.py
    first revision

---
+++ mqttClient.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("home/#")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("192.168.3.4", 1883, 60)
+
+client.loop_forever()
